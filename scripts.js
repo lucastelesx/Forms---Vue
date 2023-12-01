1a2c4e5c9ee4fbfc57e7825dec130d87a803f566
coelhoDev (Lucas Teles Rosa) <lucastelesx@users.noreply.github.com>
2023-12-01 10:17:14 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+members = [
+  {
+    fname: 'John',
+    lname: 'Lennon',
+    instrument: 'Violão'
+  },
+  {
+    fname: 'George',
+    lname: 'Harrison',
+    instrument: 'Guitarra'
+  }
+]
+
+const handlingForms = {
+  
+  data() {
+    return {
+      members: window.members,
+      newMember: {}
+    }
+  },
+  methods: {
+    addMember: function(){
+      if (this.newMember.fname && this.newMember.lname && this.newMember.instrument){
+        this.members.push(this.newMember);
+        this.newMember = {};
+      } else {
+        alert("all fields must be filled")
+      }
+    }
+  }
+  
+};
+
+Vue.createApp(handlingForms).mount('#app');
